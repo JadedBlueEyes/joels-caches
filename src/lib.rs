c9d47b0605de5f5154a5d1916b377af96cd10312
Joel Ellis <joel@ellis.link>
2020-08-25 19:53:03 +0100

    re-export caches

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,6 @@
 //! [expiring_cache] contains a cache that stores data along with a creation time (Instant) and life time (Duration). If the creation time is more than the life time, it has the status Expired, otherwise it is Found or NotFound.
 
 pub mod expiring_cache;
+
+
+pub use expiring_cache::ExpiringCache;
