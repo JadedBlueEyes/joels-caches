0fe9c521b188bb7f9278a0ffc8e88a4506473026
Joel Ellis <joel@ellis.link>
2020-08-25 19:28:00 +0100

    Move cache into different crate

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+//! This crate contains custom caches used in some of my projects.
+//!
+//! [expiring_cache] contains a cache that stores data along with a creation time (Instant) and life time (Duration). If the creation time is more than the life time, it has the status Expired, otherwise it is Found or NotFound.
+
+pub mod expiring_cache;
